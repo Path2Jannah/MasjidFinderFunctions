6b77c088cf21511e666c3e6e0d13a788db429ca8
Yahya <yahyad123@gmail.com>
2023-05-18 19:34:44 +0200

    Commit new firebase function

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -12,36 +12,79 @@ import {SalaahTimesDaily} from "./models/SalaahTimesByCity";
 //   data: Time[];
 // }
 
+interface SalaahTimingParams {
+  date: string;
+  latitude: number;
+  longitude: number;
+  method: number;
+}
+
+async function fetchSalaahTimings(params: SalaahTimingParams) {
+  try {
+    const { date, latitude, longitude, method } = params;
+    const response = await axios.get(`http://api.aladhan.com/v1/timings/${date}`, {
+      params: {
+        latitude,
+        longitude,
+        method
+      }
+    });
+    const prayerTimings = response.data.data.timings;
+    console.log(prayerTimings);
+  } catch (error) {
+    // Handle error
+    console.error('Error fetching prayer timings:', error);
+  }
+}
+
+export const getSalaahTiming = functions.https.onRequest(async (request, response) => {
+  try {
+    const params: SalaahTimingParams = {
+      date: request.query.date as string,
+      latitude: parseFloat(request.query.latitude as string),
+      longitude: parseFloat(request.query.longitude as string),
+      method: parseInt(request.query.method as string, 10)
+    };
+
+    const prayerTimings = await fetchSalaahTimings(params);
+
+    response.json(prayerTimings);
+  } catch (error) {
+    console.error('Error fetching prayer timings:', error);
+    response.status(500).send('An error occurred while fetching prayer timings.');
+  }
+});
+
 /**
  * Initiates a api request
  * @param {string} country The selected country
  * @param {string} city The selected city
  * @return {any} Returns data or error
  */
-async function getSalaahTimes(country: string, city: string) {
-  try {
-    const {data, status} = await axios.get<SalaahTimesDaily>(
-        "http://api.aladhan.com/v1/timingsByCity", {
-          params: {
-            country: country,
-            city: city,
-            method: "8",
-          },
-        },
-    );
-    console.log("response status is:", status);
+// async function getSalaahTimes(country: string, city: string) {
+//   try {
+//     const {data, status} = await axios.get<SalaahTimesDaily>(
+//         "http://api.aladhan.com/v1/timingsByCity", {
+//           params: {
+//             country: country,
+//             city: city,
+//             method: "8",
+//           },
+//         },
+//     );
+//     console.log("response status is:", status);
 
-    return data.data.timings;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      console.log("Error message: ", error.message);
-      return error.message;
-    } else {
-      console.log("unexpected error: ", error);
-      return "Unexpected error";
-    }
-  }
-}
+//     return data.data.timings;
+//   } catch (error) {
+//     if (axios.isAxiosError(error)) {
+//       console.log("Error message: ", error.message);
+//       return error.message;
+//     } else {
+//       console.log("unexpected error: ", error);
+//       return "Unexpected error";
+//     }
+//   }
+// }
 
 // /**
 //  * Initiates a api request
@@ -103,18 +146,18 @@ async function getSalaahTimes(country: string, city: string) {
 //   }
 // }
 
-export const callSalaahTimeDailyonCall = functions.https.onCall(async (
-    data, context
-) => {
-  const country = data.country;
-  const city = data.city;
-  return (await getSalaahTimes(country, city));
-});
+// export const callSalaahTimeDailyonCall = functions.https.onCall(async (
+//     data, context
+// ) => {
+//   const country = data.country;
+//   const city = data.city;
+//   return (await getSalaahTimes(country, city));
+// });
 
-export const callSalaahTimeDaily = functions.https.onRequest(async (
-    request, response
-) => {
-  const country = request.body.country;
-  const city = request.body.city;
-  response.send(await getSalaahTimes(country, city));
-});
+// export const callSalaahTimeDaily = functions.https.onRequest(async (
+//     request, response
+// ) => {
+//   const country = request.body.country;
+//   const city = request.body.city;
+//   response.send(await getSalaahTimes(country, city));
+// });
