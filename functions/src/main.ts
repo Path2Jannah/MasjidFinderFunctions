0e5d8a8536f8b8dee29413b089de1cd98b905829
Yahya <yahyad123@gmail.com>
2023-05-18 20:07:38 +0200

    Added new functions

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -7,39 +7,45 @@ interface SalaahTimingParams {
   longitude: number;
   method: number;
 }
-
+/**
+ * AddJdoc
+ * @param {SalaahTimingParams} params
+ */
 async function fetchSalaahTimings(params: SalaahTimingParams) {
   try {
-    const { date, latitude, longitude, method } = params;
+    const {date, latitude, longitude, method} = params;
     const response = await axios.get(`http://api.aladhan.com/v1/timings/${date}`, {
       params: {
         latitude,
         longitude,
-        method
-      }
+        method,
+      },
     });
     const prayerTimings = response.data.data.timings;
     console.log(prayerTimings);
   } catch (error) {
     // Handle error
-    console.error('Error fetching prayer timings:', error);
+    console.error("Error fetching prayer timings:", error);
   }
 }
 
-export const getSalaahTiming = functions.https.onRequest(async (request, response) => {
-  try {
-    const params: SalaahTimingParams = {
-      date: request.query.date as string,
-      latitude: parseFloat(request.query.latitude as string),
-      longitude: parseFloat(request.query.longitude as string),
-      method: parseInt(request.query.method as string, 10)
-    };
+export const getSalaahTiming = functions.https.onRequest(
+    async (request, response) => {
+      try {
+        const params: SalaahTimingParams = {
+          date: request.query.date as string,
+          latitude: parseFloat(request.query.latitude as string),
+          longitude: parseFloat(request.query.longitude as string),
+          method: parseInt(request.query.method as string, 3),
+        };
 
-    const prayerTimings = await fetchSalaahTimings(params);
+        const prayerTimings = await fetchSalaahTimings(params);
 
-    response.json(prayerTimings);
-  } catch (error) {
-    console.error('Error fetching prayer timings:', error);
-    response.status(500).send('An error occurred while fetching prayer timings.');
-  }
-});
+        response.json(prayerTimings);
+      } catch (error) {
+        console.error("Error fetching prayer timings:", error);
+        response.status(500).send(
+            "An error occurred while fetching prayer timings."
+        );
+      }
+    });
