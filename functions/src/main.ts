23ac1799c11dce56776b63f0afec836881317399
Yahya <yahyad123@gmail.com>
2023-05-28 15:59:09 +0200

    Add random space

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -37,7 +37,7 @@ export const closestList = functions.https.onRequest(async (req, res) => {
     const areas = uniqueAreaObj.areas.map((area:string) => area.toLowerCase);
     res.status(200).json(areas);
   } catch (error) {
-    console.error("Error reading Firestore collection:", error);
+    console.error("Error reading Firestore collection: ", error);
     res.status(500).send("Error reading Firestore collection");
   }
 });
