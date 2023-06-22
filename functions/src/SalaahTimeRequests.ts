e2fc24a755bde8e3c7067321c981c4e8d651f965
Yahya <yahya@entersekt.com>
2023-06-22 15:14:24 +0200

    Change base url and logging for error

--- functions/src/SalaahTimeRequests.ts
+++ functions/src/SalaahTimeRequests.ts
@@ -6,17 +6,17 @@ import {AxiosService} from "./services/AxiosService";
  * Class SalaahTimeRequests
  */
 export class SalaahTimeRequests {
-  private axiosService = new AxiosService("https://api.aladhan.com/v1/timings", null);
+  private axiosService = new AxiosService("https://api.aladhan.com/v1/timings/", null);
 
   public async getSalaahTimesDailyCapeTown(date: string): Promise<any> {
     this.axiosService.get(
-        `/${date}?latitude=-33.9249&longitude=18.4241`
+        `${date}?latitude=-33.9249&longitude=18.4241`
     ).then((response: any) => {
       console.log("Retrived response: ", response);
       return response;
     })
         .catch((error: any) => {
-          console.error(error);
+          console.error("Error: ", error as string);
           throw error;
         });
   }
