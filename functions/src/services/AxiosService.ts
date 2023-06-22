247370b29644b2e642850da505f8d7a3d8dc1033
Yahya <yahyad123@gmail.com>
2023-06-22 22:36:35 +0200

    Change handle error in axios

--- functions/src/services/AxiosService.ts
+++ functions/src/services/AxiosService.ts
@@ -43,7 +43,7 @@ export class AxiosService {
   }
 
   private handleError(response: AxiosResponse) {
-    console.log("Axios response error: ", response.data);
+    console.log("Axios response error: ", response);
     return response.data;
   }
 
