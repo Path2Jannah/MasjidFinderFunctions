ac9980629fe21a0779aafc9afbbda44ff8b2d4b5
Yahya <yahyad123@gmail.com>
2023-09-19 00:50:42 +0200

    Add user service:

---
+++ functions/src/services/UserInfoService.ts
@@ -0,0 +1,23 @@
+import {FirestoreService} from "./FirestoreService";
+
+/**
+ * User info service.
+ *
+ * Functions in this class will be the following:
+ * Check if the user exisits in the database.
+ * Write salaah status to database.
+ */
+export class UserInfoService {
+  /**
+     * Insance of the Firestore service
+     */
+  private userdB : FirestoreService;
+
+  /**
+   *
+   * @param {FirestoreService} userdB - The instance of the userDB.
+   */
+  constructor(userdB: FirestoreService) {
+    this.userdB = userdB;
+  }
+}
