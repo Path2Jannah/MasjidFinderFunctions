a8518e867c0ba57f8a0137bbd9dd764cab872991
yahyadollie <yahya@DESKTOP-3V81ECJ>
2023-12-14 13:41:19 +0200

    Indent problems

--- functions/src/HadithRequest.ts
+++ functions/src/HadithRequest.ts
@@ -7,19 +7,19 @@ import {AxiosService} from "./services/AxiosService";
 export class HadithRequest {
   private axiosService = new AxiosService("https://api.sunnah.com/v1/");
   private headers = {
-      "X-API-Key": "SqD712P3E82xnwOAEOkGd5JZH8s9wRR24TqNFzjk",
-    };
+    "X-API-Key": "SqD712P3E82xnwOAEOkGd5JZH8s9wRR24TqNFzjk",
+  };
 
   public async getHadith(): Promise<HadithCollection> {
-      try {
-          const res = await this.axiosService.get(
-              "hadiths/random",
-              this.headers,
-          );
-          return res.data;
-      } catch(error) {
-          console.error("Error: ", error as string);
-          throw error;
-      }
+    try {
+      const res = await this.axiosService.get(
+          "hadiths/random",
+          this.headers,
+      );
+      return res.data;
+    } catch (error) {
+      console.error("Error: ", error as string);
+      throw error;
+    }
   }
 }
