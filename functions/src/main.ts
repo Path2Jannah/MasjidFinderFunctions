e1c57db069b06450f7ad591ae9badcf85b4c8b59
yahyadollie <yahya@DESKTOP-3V81ECJ>
2024-04-24 23:44:28 +0200

    Stupid odd collection path error

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -203,7 +203,7 @@ functions.https.onRequest(async (req, res) => {
       const [fileData] = await file.download();
       const jsonData: HadithJson = JSON.parse(fileData.toString());
 
-      const firebaseCollection = new FirestoreService(firestoreDatabase, `/HadithCollection/ddfbd6e6-ecfa-4081-8bdd-adcf6335bcfc/HadithCompilers/1/Books/1/hadith/${i}`);
+      const firebaseCollection = new FirestoreService(firestoreDatabase, `/HadithCollection/ddfbd6e6-ecfa-4081-8bdd-adcf6335bcfc/HadithCompilers/1/Books/1/hadith/${i}/`);
       const documentObject = {
         chapter_id: jsonData.chapterId,
         chapter_num: jsonData.chapterNumber,
