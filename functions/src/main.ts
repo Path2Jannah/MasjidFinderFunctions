350c1a76808ab250c400f6029b9846aa65fabe11
yahyadollie <yahya@DESKTOP-3V81ECJ>
2024-05-18 11:03:46 +0200

    Add logs

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -266,9 +266,13 @@ functions.https.onRequest(async (req, res) => {
     if (bookNumber == null) {
       const firebaseCollection = new FirestoreService(firestoreDatabase, `/HadithCollection/ddfbd6e6-ecfa-4081-8bdd-adcf6335bcfc/HadithCompilers/${collectionId}/Books`);
       const numberOfBooks = await firebaseCollection.getBookNumber;
+      console.log("Number of books", numberOfBooks.length)
       for (let i=1; i < numberOfBooks.length; i++) {
+        console.log("Proccessing book", i)
         const allHadithInBook = getHadithData(collectionId, i);
+        console.log((await allHadithInBook).at(1));
         (await allHadithInBook).forEach((hadith) => {
+          console.log("HadithId", hadith.hadithNumber);
           hadithData.push(hadith);
         });
       }
